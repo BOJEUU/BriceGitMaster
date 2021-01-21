f21882bbd4124156dd203555a9c9609eb8271741
Celine_lhp5 <celine.louvel.76@outlook.fr>
2021-01-21 16:49:38 +0100

    ajout céline

---
+++ datas/sw-data.php
@@ -0,0 +1,41 @@
+<?php
+
+    $sw_data = [
+        [
+            "name" => "Arc-170",
+            "image" => "images/arc170.jpg",
+            "size" => "14.5m",
+            "weight" => "1 tonne",
+            "arsenal" => "2 canons lasers moyens, 2 canons lasers de poupe, 1 lance-torpilles à protons (stock de 6)",
+            "astromechDroid" => true
+        ],
+
+        [
+            "name" => "Chasseur Gantelet",
+            "image" => "images/gauntlet_fighter.jpg",
+            "size" => "52.3m",
+            "weight" => "5 tonnes",
+            "arsenal" => "2 canons lasers avant, 2 canons laser latéraux",
+            "astromechDroid" => false
+        ],
+
+        [
+            "name" => "Intercepteur Impérial Fureur",
+            "image" => "images/intercepteur_fureur.jpg",
+            "size" => "50m",
+            "weight" => "4.5 tonnes",
+            "arsenal" => "2 doubles canons lasers",
+            "astromechDroid" => false
+        ],
+
+        [
+            "name" => "U-Wing UT-60D",
+            "image" => "images/u-wing.png",
+            "size" => "24,98 m avec les ailes repliées, 3,35 m de hauteur",
+            "weight" => "3 tonnes",
+            "arsenal" => "2 canons lasers",
+            "astromechDroid" => false
+        ],
+    ];
+
+?>
