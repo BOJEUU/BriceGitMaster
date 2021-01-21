646204f97cad38cc9363645c3e872cc564ee14f6
brice766 <briere.brice@gmail.com>
2021-01-21 16:42:24 +0100

    brice

---
+++ datas/sw-data.php
@@ -0,0 +1,40 @@
+<?php 
+
+$sw_data = [
+    [
+        "name" => "Razor Crest",
+        "image" => "images/razor_crest.jpg",
+        "size" => "13.5m",
+        "weigth" => "2 tonnes",
+        "arsenal" => "8 batteries turbolasers légères, 20 canons à ions lourds, 12 canons lasers",
+        "astromechDroid" => false
+    ],
+
+    [
+        "name" => "Imperial-II Star Destroyer",
+        "image" => "images/destroyer_imperial2",
+        "size" => "1600 m",
+        "weigth" => "9653 tonnes",
+        "arsenal" => "8 batteries octuples de turbolasers, 50 batteries turbolasers lourdes, 50 batteries turbolasers légères, 20 canons à ions lourds, 10 rayons tracteurs Phylon Q7, 26 canons lasers",
+        "astromechDroid" => true
+    ],
+
+    [
+        "name" => "TIE Chasseur",
+        "image" => "images/Chasseur_TIE",
+        "size" => "7.80 mètres",
+        "weigth" => "1 tonne",
+        "arsenal" => "2 canons laser SFS L-s1 ; 2 lance-missiles à concussion SFS M-s3 (4 en magasin) ; 2 lance-torpilles à protons SFS T-s5 (2 en magasin) ; Assortiments de Bombes à Protons, Mines Orbitales et Détonateurs Thermiques",
+        "astromechDroid" => false
+    ],
+
+    [
+        "name" => "Millennium Falcon",
+        "image" => "images/faucon",
+        "size" => "34,75 m",
+        "weigth" => "199 513 tonnes",
+        "arsenal" => "2 canons quad laser corelliens AG-2G modifiés ; 1 canon blaster de défense à la surface BlasTech AX-108 ; 2 lanceurs de missiles à concussion Arakyd ST2",
+        "astromechDroid" => false
+    ],
+];
+?>
