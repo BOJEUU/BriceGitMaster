d1f8963974e7e2de34b76ffbaa5803c61fdff074
wengervictor329@gmail.com <wengervictor329@gmail.com>
2021-01-21 16:38:44 +0100

    push victor

---
+++ datas/sw-data.php
@@ -0,0 +1,40 @@
+<?php 
+ $sw_data = [
+     [
+         "name" => "Ebon Hawk",
+         "size" => "24m",
+         "weight" => "154500kg",
+         "arsenal" => "Tourelle turbolaser double dorsale; Double canon laser lourd sur chaque aile; Blaster à répétition anti-personnel ventral",
+         "image" => "images/ebonHawk.jpg",
+         "astromechDroid" => false
+     ]
+     ,
+     [
+        "name" => "A-wing",
+        "size" => "9,60 mètres de long ; 6,48 mètres de large ; 3,11 mètres de haut",
+        "weight" => "75000kg",
+        "arsenal" => "deux canons lasers pivotants Borstel RG9  deux lances-missiles à concussion Dymek HM-6 (magasins de six)",
+        "image" => "images/A-wing.png",
+        "astromechDroid" => true
+    ]
+    ,[
+        "name" => "V-wing",
+        "size" => "7.9m",
+        "weight" => "50000kg",
+        "arsenal" => "2 doubles canons laser",
+        "image" => "images/V-wing.png",
+        "astromechDroid" => false
+    ]
+    ,
+    [
+        "name" => "Mante D-5",
+        "size" => "68m de longueur, 74m de largeur, 58m de hauteur",
+        "weight" => "250000kg",
+        "arsenal" => "Canons rétractables",
+        "image" => "images/mante.png",
+        "astromechDroid" => false
+    ]
+     
+ ]
+ 
+ ?>
