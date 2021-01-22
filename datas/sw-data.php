0eda43309ae535db5ebd4928127d1dbc76c85d95
ChrisLamanu <christopher.vrel@gmail.com>
2021-01-22 09:45:27 +0100

    "Chris add"

---
+++ datas/sw-data.php
@@ -0,0 +1,36 @@
+<?php
+    $sw_data = [
+        [
+            "name" => "Slave I",
+            "size" => "21.5m",
+            "weight" => "122650kg",
+            "arsenal" => "2 canons blaster rotatifs, 2 canons laser (dissimulés), 2 lance-torpilles à proton (dissimulés), 1 poseur de mines spatial (dissimulé) // Boba Fett : 2 canons blaster rotatifs, 1 canon à ions (dissimulé), 1 lance-missiles à concussion (dissimulé), 2 lance-torpilles à proton (dissimulés), 1 projecteur de rayon tracteur (dissimulé)",
+            "image" => "images/slaveOne.jpg",
+            "astromechDroid" => false
+        ],
+        [
+            "name" => "X-Wing T-65",
+            "size" => "12.5m",
+            "weight" => "25172kg",
+            "arsenal" => "4 canons lasers Tain & Bak KX9 ou IX4; 2 lance-torpilles à proton Krupx MG7",
+            "image" => "images/X-WingT-65.jpg",
+            "astromechDroid" => true
+        ],
+        [
+            "name" => "Naboo N-1",
+            "size" => "11m",
+            "weight" => "18716kg",
+            "arsenal" => "2 canons blasters, 2 Tubes à proton de 10 Torpilles chacun",
+            "image" => "images/NabooN-1.jpg",
+            "astromechDroid" => true
+        ],
+        [
+            "name" => "Corvette Hammerhead",
+            "size" => "116.7m",
+            "weight" => "886930kg",
+            "arsenal" => "2 canons laser",
+            "image" => "images/Endarconcept.jpg",
+            "astromechDroid" => false
+        ]
+    ];
+?>
