8494396bfcabbbf0caaa8d0a36759af6f84e50d7
Raphoudre <raphael.ivol@hotmail.fr>
2021-01-22 09:21:42 +0100

    raph

---
+++ datas/sw-data.php
@@ -0,0 +1,38 @@
+<?php
+
+$sw_data = [
+    [
+        "name" => "Destroyer Stellaire Venator"
+        "image" => "images/destroyer_venator.jpg"
+        "size" => "1137 m de longueur pour 548 m de large et 268m de haut"
+        "weight" => "Vaisseau lourd"
+        "arsenal" => "8 turbolasers lourds, 2 turbolasers moyens, 52 canons lasers, 6 rayons tracteurs, 4 lance-torpilles à proton"
+        "astromecano" => "Sans"
+    ];
+    [
+        "name" => "Twilight"
+        "image" => "images/twilight.jpg"
+        "size" => "34,1 m de long pour 16,97 m de large et 15,73 m de haut"
+        "weight" => "Vaisseaux légers"
+        "arsenal" => "3 Blasters lourds / Tourelles laser rotatives"
+        "astromecano" => "Sans"
+    ];
+    [
+        "name" => "Tydirium"
+        "image" => "tyridium.jpg"
+        "size" => "none"
+        "weight" => "Vaisseaux légers"
+        "arsenal" => "none"
+        "astromecano" => "none"
+    ];
+    [
+        "name" => "Valance"
+        "image" => "images/valance.jpg"
+        "size" => "none"
+        "weight" => "Vaisseaux légers"
+        "arsenal" => "none"
+        "astromecano" => "none"
+    ];
+];
+
+?>
